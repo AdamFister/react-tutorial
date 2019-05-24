b50cfb29f4e7a16071acfb29240584a6aa85fbf8
Adam Fister <arfister@gmail.com>
2019-05-24 09:50:07 -0400

    state, submitting form data

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
+
+    state = {
+        characters: [
+
+        ],
+    };
+    handleSubmit = character => {
+        this.setState({ characters: [...this.state.characters, character] })
+      }
+
+    removeCharacter = index => {
+        const { characters } = this.state
+      
+        this.setState({
+          characters: characters.filter((character, i) => {
+            return i !== index
+          }),
+        })
+      };
+
     render() {
-        const characters = [
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-            },
-            {
-                name: 'Mac',
-                job: 'Bouncer',
-            },
-            {
-                name: 'Dee',
-                job: 'Aspring actress',
-            },
-            {
-                name: 'Dennis',
-                job: 'Bartender',
-            },
-        ]
         return (
             <div className="container">
-                <Table characterData={characters} />
+              <Table characterData={this.state.characters} removeCharacter={this.removeCharacter} />
+              <Form handleSubmit={this.handleSubmit} />
             </div>
-
-        )
+          )
     }
+    
+
 }
+
+
+
 
 export default App
